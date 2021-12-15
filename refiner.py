741c083c08d9c6b2434661fe62abc70508c4f5eb
ElkeDeZitter <elke.dezitter@gmail.com>
2021-12-15 18:08:26 +0100

    resolve bug again

--- refiner.py
+++ refiner.py
@@ -1497,7 +1497,7 @@ def run(args):
             if mp in ('qFextr_map','qFgenick_map','qFextr_calc_map'):
                 dir_prefix = 'qweight_occupancy'
             elif mp in ('kFextr_map','kFgenick_map','kFextr_calc_map'):
-                dir_prefix = 'qweight_occupancy'
+                dir_prefix = 'kweight_occupancy'
             else:
                 dir_prefix = 'occupancy'
 
@@ -1587,11 +1587,11 @@ def run(args):
                 model_label='%s_real_space'%(mp_type)
                 ccp4_map_label='%s'%(mp)
             if len(ccp4_list) == len(pymol_pdb_list) == len(Xtrapol8_params.occupancies.list_occ):
-                print("Pymol movie residue list: %s" %(DH.residue_list))
+                #print("Pymol movie residue list: %s" %(DH.residue_list))
                 PM = Pymol_movie(Xtrapol8_params.occupancies.list_occ, pdblst=pymol_pdb_list, ccp4_maps = ccp4_list,
                              model_label=model_label, ccp4_map_label=ccp4_map_label, resids_lst = DH.residue_list)
             else:
-                print("Pymol movie residue list: %s" %(DH.residue_list))
+                #print("Pymol movie residue list: %s" %(DH.residue_list))
                 PM = Pymol_movie(Xtrapol8_params.occupancies.list_occ, pdblst=pymol_pdb_list, model_label=model_label, resids_lst = DH.residue_list)
             PM.write_pymol_script(outfile=pymol_script_out)
                 
