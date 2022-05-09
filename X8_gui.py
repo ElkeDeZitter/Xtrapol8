7e36de1940ddcc225d00c860852b6fb444cb3545
ElkeDeZitter <elke.dezitter@gmail.com>
2022-05-09 11:23:22 +0200

    debug renamed map-explorer parameters

--- X8_gui.py
+++ X8_gui.py
@@ -648,8 +648,8 @@ class MainFrame(wx.Frame):
         else:
             tabExt.missChoice.SetSelection(0)
 
-        tabExt.ThreshTextCtrl.SetValue(str(user_params.map_explorer.peak_integration_floor))
-        tabExt.PeakTextCtrl.SetValue(str(user_params.map_explorer.peak_detection_threshold))
+        tabExt.peak_detection_thresholdTextCtrl.SetValue(str(user_params.map_explorer.peak_detection_threshold))
+        tabExt.peak_integration_floorTextCtrl.SetValue(str(user_params.map_explorer.peak_integration_floor))
         tabExt.RadiusTextCtrl.SetValue(str(user_params.map_explorer.radius))
         tabExt.ZscoreTextCtrl.SetValue(str(user_params.map_explorer.z_score))
         tabExt.DistanceAnalysis.SetValue(user_params.map_explorer.use_occupancy_from_distance_analysis)
@@ -929,8 +929,8 @@ class MainFrame(wx.Frame):
         ##############################
         ### Map explorer - Ext_tab ###
         ##############################
-        tobeparsed += "map_explorer.peak_integration_floor = %s\n" % self.get_txtctrl_values(tabExt.ThreshTextCtrl) + \
-                      "map_explorer.peak_detection_threshold = %s\n" % self.get_txtctrl_values(tabExt.PeakTextCtrl) + \
+        tobeparsed += "map_explorer.peak_integration_floor = %s\n" % self.get_txtctrl_values(tabExt.peak_integration_floorTextCtrl) + \
+                      "map_explorer.peak_detection_threshold = %s\n" % self.get_txtctrl_values(tabExt.peak_detection_thresholdTextCtrl) + \
                       "map_explorer.radius = %s\n" % self.get_txtctrl_values(tabExt.RadiusTextCtrl) + \
                       "map_explorer.z_score = %s\n" % self.get_txtctrl_values(tabExt.ZscoreTextCtrl) + \
                       "map_explorer.use_occupancy_from_distance_analysis = %s\n" % tabExt.DistanceAnalysis.GetValue()
