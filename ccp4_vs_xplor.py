18d11e026b0a5b0bb86ee338b16317e9f8db17e9
Nicolas Coquelle <nicolascoquelle62@hotmail.com>
2022-10-04 11:01:37 +0200

    map explorer with Map objects. Coordinate transformation for any SG

--- ccp4_vs_xplor.py
+++ ccp4_vs_xplor.py
@@ -1,25 +1,16 @@
 import iotbx.xplor.map
 from iotbx import ccp4_map
 import numpy as np
-map_name = "/Users/coquelle/IBS_2022/RS_SwissFEL/Fext_grad/FoFo_dark/1ps_7uJ/Fextr_grad_MC_mqFoFo.map"
-map_ccp4 = "/Users/coquelle/IBS_2022/RS_SwissFEL/Fext_grad/FoFo_dark/1ps_7uJ/Fextr_grad_MC_mqFoFo.ccp4"
+map_ccp4 = "/Users/coquelle/IBS_2022/RS_SwissFEL/Fext_grad/1ps_2uJ_MC_2/Fextr_grad_MC_mqFoFo.ccp4"
+#map_ccp4 = "/Users/coquelle/IBS_2022/RS_SwissFEL/Fext_grad/FoFo_dark/1ps_7uJ/Fextr_grad_MC_mqFoFo.ccp4"
 pdb = "/Users/coquelle/IBS_2022/RS_SwissFEL/inputs/newdarkjpnew_001.pdb"
-xplor = iotbx.xplor.map.reader(file_name=map_name)
-from iotbx.file_reader import any_file
-
-any = any_file(file_name=map_ccp4)
 ccp4 = ccp4_map.map_reader(file_name=map_ccp4)
 
 
 
 
-#grid = np.array(xplor.gridding.n, dtype=np.float32)
-#first = np.array(xplor.gridding.first, dtype=np.float32)
-#unit_cell = np.array(xplor.unit_cell.parameters(), dtype=np.float32)
-data_xplor = xplor.data.as_numpy_array()
 
 
-print(any.file_object)
 
 grid = np.array(ccp4.unit_cell_grid, dtype=np.float32)
 print(grid)
@@ -27,7 +18,6 @@ first = np.array(ccp4.data.as_double().origin(), dtype=np.float32)
 print(first)
 unit_cell = np.array(ccp4.unit_cell_parameters, dtype=np.float32)
 print(unit_cell)
-#data = map_data(ccp4)
 
 n_real = ccp4.unit_cell_grid
 print(n_real)
@@ -35,15 +25,7 @@ data = ccp4.data.as_numpy_array()
 print("ccp4 shape")
 print(data.shape)
 
-diff = data - data_xplor
-print(diff.min(), diff.max(), diff.mean(), diff.std())
 
 from map_explorer import map_explorer
-map_explorer(map_ccp4, pdb, 1.5, 3.0, 4.0, 'ccp4')
-
-map_explorer(map_name, pdb, 1.5, 3.0, 4.0, 'xplor')
-
-#plt.hist(data.flatten(), "auto") #, weights=counts)
-#plt.show()
-
+map_explorer(map_ccp4, pdb, 1.5, 3.0, 4.5, 'ccp4')
 
--- map_explorer.py
+++ map_explorer.py
@@ -22,31 +22,12 @@ import sys
 import os
 import numpy as np
 import iotbx.xplor.map
+from iotbx import ccp4_map
 from scipy import ndimage
 from iotbx.pdb import hierarchy
 
-#TODO: convert to usage of iotbx instead of reading pdb as text file
 
-from iotbx import ccp4_map
-from iotbx import file_reader
-
-def map_data(map_obj):
-    m_data = map_obj.data.as_double()
-    n_real = map_obj.unit_cell_grid
-    if(n_real == m_data.all()):
-        return map_obj.data.as_numpy_array() / map_obj.header_rms
-    else:
-      # XXX hideously SLOW! MOVE TO C++
-      # map_new = flex.double(flex.grid(n_real), 0)
-      map_new = np.empty(n_real)
-      o = m_data.origin()
-      f = m_data.focus()
-      for i in range(o[0],f[0]):
-        for j in range(o[1],f[1]):
-          for k in range(o[2],f[2]):
-            map_new[i%n_real[0], j%n_real[1], k%n_real[2]] = m_data[i, j, k] / map_obj.header_rms
-      return map_new
-
+# TODO: convert to usage of iotbx instead of reading pdb as text file
 def get_atom_info(pdb):
     """
     This function return a list of the coordinates and info of each atom
@@ -76,150 +57,48 @@ def get_atom_info(pdb):
     return np.array(coord), info
 
 
-#Following definitions have to be changed towards iotbx.pdb.hierarchy as to read non-standard pdb files (and mmcif)
-def getAtomSerialNumber(line):
-    """
-    integer
-    """
-    return int(line[6:11])
-
-def getAtomName(line):
-    return line[12:16]
-
-def getAltLoc(line):
-    return line[16]
-
-def getResidueName(line):
-    return line[17:20]
-
-def getChainID(line):
-    if line[20] == ' ':
-        chainID = line[21]
-    else:
-        chainID = line[20]
-    return chainID
-
-def getResidueNumber(line):
-    return line[22:26]
-
-
-#Not used in this script
-def getICode(line):
-    return line[26]
-
-def getCoordinates(line):
-    """
-    Each float is of format 8.3
-    """
-    return float(line[30:38]), float(line[38:46]), float(line[46:54])
-
-#not used in this script
-def getOccupancy(line):
-    """
-    f6.2
-    """
-    return float(line[54:60])
-
-#Function in which it is used is not used in this script
-def getBFactor(line):
-    """
-    f6.2
-    """
-    return float(line[60:66])
-
-#Not used in this script
-def getElement(line):
-    return line[76:78]
-
-#Not used in this script
-def getCharge(line):
-    return line[78:80]
-
-def get_d(P1,P2, axis=0):
+def get_d(P1, P2, axis=0):
     """
     :param P1: tuple of coordinates x1,y1,z1 (float)
     :param P2: tuple of coordinates x2,y2,z2 (float)
+    :param axis: axis along which computing the sum (0 by default, should not be changed)
     :return: the distance between these two points
     """
+    return np.sqrt(np.sum((P1-P2)**2, axis=axis))
 
-    return np.sqrt(np.sum((P1-P2)**2,axis=axis))
 
-#Not used in this script
-def get_B(pdb):
-    lines = open(pdb).readlines()
-    Bs = []
-    for line in lines:
-        if line.split()[0] in ['ATOM', 'HETATM']:
-            Bs.append(getBFactor(line))
-    return np.array(Bs)
-
-#Not used in this script
-def get_coord_via_PyMOL(pdb):
-    cmd.load("%s"%pdb,"mymol")
-    #cmd.select("A", "mymol and alt A")
-    stored.coord = []
-    stored.info = []
-    res = []
-    cmd.iterate_state(1, selector.process("mymol"), "stored.coord.append([x,y,z])")
-    cmd.iterate_state(1, selector.process("mymol"), "stored.info.append((resn, resv, chain, alt, name, ID))")
-
-    cmd.iterate("mymol and ( (not alt B and name CA) or (resn HOH))", "res.append((resv, resn))", space={'res': res} )
-    return np.array(stored.coord), stored.info
-
-def get_coord_from_parser(pdb):
-    lines = open(pdb).readlines()
-    coord = []
-    info = []
-    for line in lines:
-        if line.split()[0] in ['ATOM','HETATM']:
-            coord.append(getCoordinates(line))
-            info.append((getResidueName(line),getResidueNumber(line),getChainID(line),getAltLoc(line),getAtomName(line),str(getAtomSerialNumber(line))))
-    return np.array(coord), info
-
-def get_coord_from_map_indices(indices, first, grid, cell_axes,verbose=False ):
-    index = indices + first
-    cartesian = index / grid * cell_axes
-    if verbose:
-        print('Indices %3i %3i %3i converted to x,y,z: %8.2f %8.2f %8.2f'%(tuple(indices)+tuple(cartesian)))
-    return cartesian
-
-#not used in this script
-def get_indices_from_coord(coord,origin_xyz,steps_xyz, verbose=False):
-    indices =  np.abs( coord - origin_xyz ) / steps_xyz
-    return np.rint(indices).astype(np.int32)
-
-def blob_detection(out, residlst, xplor, threshold, peak, coord, radius,info):
+def blob_detection(out, residlst, map_object, threshold, peak, coord, radius,info):
     pos = []
     neg = []
-    try:
-      grid  = np.array(xplor.gridding.n, dtype=np.float32)
-      first = np.array(xplor.gridding.first, dtype=np.float32)
-      unit_cell = np.array(xplor.unit_cell.parameters(), dtype=np.float32)
-      data = xplor.data.as_numpy_array()  
-    except:
-      grid = np.array(xplor.unit_cell_grid, dtype=np.float32)
-      first = np.array(xplor.data.as_double().origin(), dtype=np.float32)
-      unit_cell = np.array(xplor.unit_cell_parameters, dtype=np.float32) 
-      data = map_data(xplor)
-    map_param = (grid, first, unit_cell)
-    pos.append(do_blob_search(data, threshold, peak, map_param, coord, radius,info))
+    map_object.open_map()
+    #try:
+    #  grid  = np.array(xplor.gridding.n, dtype=np.float32)
+    #  first = np.array(xplor.gridding.first, dtype=np.float32)
+    #  unit_cell = np.array(xplor.unit_cell.parameters(), dtype=np.float32)
+    #  data = xplor.data.as_numpy_array()
+    #except:
+    #  grid = np.array(xplor.unit_cell_grid, dtype=np.float32)
+    #  first = np.array(xplor.data.as_double().origin(), dtype=np.float32)
+    #  unit_cell = np.array(xplor.unit_cell_parameters, dtype=np.float32)
+    #  data = xplor.data.as_numpy_array()
+    #map_param = (grid, first, unit_cell)
+    pos.append(do_blob_search(map_object, threshold, peak, coord, radius, info))
     print_results(pos, out, residlst)
-    neg.append(do_blob_search(data, -threshold, peak, map_param, coord, radius,info))
+    neg.append(do_blob_search(map_object, -threshold, peak, coord, radius, info))
     print_results(neg, out, residlst)
     return pos+neg
 
-def do_blob_search(data, threshold, peak, map_param, coord_pdb, radius, info):
+def do_blob_search(map_object, threshold, peak, coord_pdb, radius, info):
 
-        grid, first, unit_cell = map_param
-        s=ndimage.morphology.generate_binary_structure(3,3)
+        s = ndimage.morphology.generate_binary_structure(3,3)
         if threshold < 0:
-            copy = np.where(data <= threshold, 1, 0)
-            copy_peak = np.where(data <= -peak, 1, 0)
-            bla = ('negative','below')
+            copy = np.where(map_object.data <= threshold, 1, 0)
+            copy_peak = np.where(map_object.data <= -peak, 1, 0)
+            bla = ('negative', 'below')
         else:
-            copy = np.where(data >= threshold, 1, 0)
-            copy_peak = np.where(data >= peak, 1, 0)
-            bla = ('positive','above')
+            copy = np.where(map_object.data >= threshold, 1, 0)
+            copy_peak = np.where(map_object.data >= peak, 1, 0)
+            bla = ('positive', 'above')
 
         labeled_array, num_features = ndimage.label(copy, structure=s)
         labeled_array_peak, num_features_peak = ndimage.label(copy_peak, structure=s)
@@ -232,9 +111,11 @@ def do_blob_search(data, threshold, peak, map_param, coord_pdb, radius, info):
             atom = []
 
             feature_to_integrate = labeled_array[np.nonzero(labeled_array_peak == i)][0]
-            blob = data[np.nonzero(labeled_array == feature_to_integrate)]
+            indices = np.nonzero(labeled_array == feature_to_integrate)
+            blob = map_object.data[indices]
 
-            if np.abs(blob).max() < peak or blob.size < 2 : continue
+            if np.abs(blob).max() < peak or blob.size < 2:
+                continue
 
             sum = blob.sum()
             size = blob.size
@@ -245,20 +126,18 @@ def do_blob_search(data, threshold, peak, map_param, coord_pdb, radius, info):
                 max = blob.max()
                 argmax = blob.argmax()
             indices = np.transpose(np.nonzero(labeled_array == feature_to_integrate))[argmax]
-            coord_map = get_coord_from_map_indices(indices, first, grid,unit_cell[0:3])
+            coord_map = map_object.get_coord_from_map_indices(indices)
 
             D = get_d(coord_pdb, coord_map, axis=1)
 
             if coord_pdb is not None:
                 if (radius is not None and D.min() <= radius) or radius is None:
                     atom.extend(info[D.argmin()])
-                    print(info[D.argmin()])
-                    #cmd.iterate('mymol and ID %i'%D.argmin(), 'atom.extend((resn, resv, chain, alt, name, ID))', space = {'D': D, 'atom': atom})
+                    print(info[D.argmin()], indices)
                     atom.extend([D.min(), max, sum, size])
-                    #print max, coord_map
+            else:
+                atom.extend((max, sum, size))
 
-
-            else: atom.extend((max, sum, size))
             if atom:
                 if coord_pdb is not None and len(atom) == 10: all_atoms.append(tuple(atom))
                 else: all_atoms.append(tuple(atom))
@@ -267,18 +146,18 @@ def do_blob_search(data, threshold, peak, map_param, coord_pdb, radius, info):
         del copy_peak
         # Nasty but remove duplicate / Use of a decorator ?
         all_atoms = np.array(list(set(tuple(all_atoms))))
-        #all_atoms = np.array(all_atoms)
         if all_atoms.size > 0:
-            sorted_indices = np.argsort(all_atoms[:,8].astype(np.float32))
+            sorted_indices = np.argsort(all_atoms[:, 8].astype(np.float32))
             if threshold < 0:
                 return all_atoms[sorted_indices]
             else:
                 all_atoms = all_atoms[sorted_indices]
                 return all_atoms[::-1]
 
-        else: return all_atoms
+        else:
+            return all_atoms
     
-def check_inputs(map, pdb, radius, threshold, peak, log):
+def check_inputs(map_name, pdb, radius, threshold, peak, log):
     """
     peak: peak_detection_threshold (sigma)
     hreshold: peak_integration_floor (sigma)
@@ -293,19 +172,21 @@ def check_inputs(map, pdb, radius, threshold, peak, log):
             coord, info = get_atom_info(pdb)
     else: coord = None
 
-    if not os.path.isfile(map):
-        print("Sorry, no such map file:%s"%(map),file=log)
-        print("Sorry, no such map file:%s"%(map))
+    if not os.path.isfile(map_name):
+        print("Sorry, no such map file:%s" % map_name, file=log)
+        print("Sorry, no such map file:%s" % map_name)
         sys.exit(1)
     else:
-        try:
-            xplor = iotbx.xplor.map.reader(file_name=map)
-        except:
+        if map_name.endswith('ccp4'):
+            map_object = CCP4_Maps(map_name)
+            #xplor = iotbx.xplor.map.reader(file_name=map)
+        else:
             try:
-              xplor = ccp4_map.map_reader(file_name=map)
+              map_object = XPLOR_Maps(map_name) \
+              #    = ccp4_map.map_reader(file_name=map)
             except:
-              print("Sorry, %s map is not a valid XPLOR or CCP4 map. Aborting map explorer."%(map), file=log)
-              print("Sorry, %s map is not a valid XPLOR or CCP4 map. Aborting map explorer."%(map))
+              print("Sorry, %s map is not a valid XPLOR or CCP4 map. Aborting map explorer." % map, file=log)
+              print("Sorry, %s map is not a valid XPLOR or CCP4 map. Aborting map explorer." % map)
               sys.exit(1)
 
     if radius is not None and coord is None:
@@ -315,9 +196,8 @@ def check_inputs(map, pdb, radius, threshold, peak, log):
 
     if radius is not None:
         radius = float(radius)
-        #try: radius = float(radius[0])
-        #except TypeError: radius = float(radius)
-    return xplor, threshold, peak, coord, radius, info
+
+    return map_object, threshold, peak, coord, radius, info
 
 def print_results(blobs, out, residlst):
     residIDlst = []
@@ -352,3 +232,90 @@ def map_explorer(map, pdb, radius, peak_integration_floor, peak_detection_thresh
     residlst.close()
     return outname
     
+class Maps(object):
+
+    def __init__(self, map_name):
+        self.map_name = map_name
+        # map origin
+        self.origin = [0, 0, 0]
+        # grid is the total number of points along each unit cell axis
+        self.grid = [0, 0, 0]
+        # unit cell parameters
+        self.alpha = 90.
+        self.beta = 90.
+        self.gamma = 90.
+        self.a = 0.
+        self.b = 0.
+        self.c = 0.
+
+    def open_map(self):
+        """
+        Overwritten in sub-classes
+        """
+        return
+
+    def coord_transform_setup(self):
+        self.a, self.b, self.c, self.alpha, self.beta, self.gamma = self.unit_cell
+        self.alpha = np.deg2rad(self.alpha)
+        self.beta = np.deg2rad(self.beta)
+        self.gamma = np.deg2rad(self.gamma)
+        print(self.a)
+        self.cosa_star = (np.cos(self.beta) * np.cos(self.gamma) - np.cos(self.alpha)) / (np.sin(self.beta) * np.sin(self.gamma))
+        print(self.cosa_star)
+        print(self.c)
+        print(self.beta)
+
+        self.sina_star = np.sqrt(1 - self.cosa_star ** 2)
+        self.transfo_matrix = np.array([[ self.a, self.b * np.cos(self.gamma), self.c * np.cos(self.beta)],
+                                        [0,       self.b * np.sin(self.gamma), - self.c * self.cosa_star * np.sin(self.beta)],
+                                        [0,       0,                           self.c * np.sin(self.beta)*self.sina_star]
+                                        ])
+
+    def get_coord_from_map_indices(self, indices, verbose=False):
+        """
+        Will return cartesian coordinates from grid map indices
+        first is the map origin (int array (3,))
+        grid is the number of map points covering a unit cell (int array (3,))
+        cell_axes is the unit cell parameters a, b and c (float array (3,))
+        WARNING: Does not work properl
+        """
+        index = indices + self.origin
+        cartesian = np.matmul(index / self.grid, self.transfo_matrix)
+        if verbose:
+            print('Indices %3i %3i %3i converted to x,y,z: %8.2f %8.2f %8.2f' % (tuple(indices) + tuple(cartesian)))
+        return cartesian
+
+    # not used in this script
+    def get_indices_from_coord(self, coord, origin_xyz, steps_xyz, verbose=False):
+        indices = np.abs(coord - origin_xyz) / steps_xyz
+        return np.rint(indices).astype(np.int32)
+
+
+class CCP4_Maps(Maps):
+
+    def __init__(self, map_name):
+        super(CCP4_Maps, self).__init__(map_name)
+
+    def open_map(self):
+        self.map_object = ccp4_map.map_reader(file_name=self.map_name)
+        self.grid = np.array(self.map_object.unit_cell_grid, dtype=np.float32)
+        self.origin = np.array(self.map_object.data.as_double().origin(), dtype=np.float32)
+        self.unit_cell = np.array(self.map_object.unit_cell_parameters, dtype=np.float32)
+        self.data = self.map_object.data.as_numpy_array()
+        self.coord_transform_setup()
+
+
+class XPLOR_Maps(Maps):
+
+    def __init__(self, map_name):
+        super(XPLOR_Maps, self).__init__(map_name)
+
+    def open_map(self):
+        self.map_object = ccp4_map.map_reader(file_name=self.map_name)
+        self.grid = np.array(self.map_object.unit_cell_grid, dtype=np.float32)
+        self.origin = np.array(self.map_object.data.as_double().origin(), dtype=np.float32)
+        self.unit_cell = np.array(self.map_object.unit_cell_parameters, dtype=np.float32)
+        self.data = self.map_object.data.as_numpy_array()
+        self.coord_transform_setup()
+
+
