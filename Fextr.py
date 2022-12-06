0a8ad462f318bfc09e424b47338bdb1358287d7e
ElkeDeZitter <elke.dezitter@gmail.com>
2022-12-06 11:21:49 +0100

    update version number

--- Fextr.py
+++ Fextr.py
@@ -1977,7 +1977,7 @@ def run(args):
             break
     global log
     log = open(logname, "w")
-    version = "1.0.2"
+    version = "1.1.0"
     print("Xtrapol8 -- version %s -- run date: %s" %(version, now), file=log)
     print('-----------------------------------------')
     print("Xtrapol8 -- version %s -- run date: %s" %(version, now))
