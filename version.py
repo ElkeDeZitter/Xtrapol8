40d9beff349c4498660fb1860056e49ff5e90cdf
ElkeDeZitter <elke.dezitter@gmail.com>
2024-10-01 12:43:31 +0200

    update version

--- version.py
+++ version.py
@@ -27,9 +27,9 @@ __citation__ = "doi.org/10.1038/s42003-022-03575-7"
 
 major = 1
 minor = 2
-micro = 6
-tag = "Dev" #can be None or "Dev"
-# tag = None #can be None or "Dev"
+micro = 7
+#tag = "Dev" #can be None or "Dev"
+tag = None #can be None or "Dev"
 
 VERSION = ".".join(map(lambda x: str(x) , [major, minor, micro]))
 if tag != None:
