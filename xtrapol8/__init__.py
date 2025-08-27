e145c061a8db75b21b5713b8faec17f4a601ee1f
Paul Bond <git@paulsbond.co.uk>
2025-08-27 14:36:10 +0200

    Changed to pyproject.toml project

---
+++ xtrapol8/__init__.py
@@ -0,0 +1,20 @@
+"Xtrapol8"
+
+MAJOR = 1
+MINOR = 2
+PATCH = 9
+
+__version_date__ = "28 Jun 2025"
+__version_info__ = (MAJOR, MINOR, PATCH)
+__version__ = ".".join(map(str, __version_info__))
+
+__authors__ = [
+    "Elke De Zitter",
+    "Nicolas Coquelle",
+    "Paula Oeser",
+    "Thomas Barends",
+    "Jacques Philippe Colletier",
+]
+__license__ = "MIT"
+__copyright__ = "Institut de Biologie Structurale - group DYNAMOP - team SNaX"
+__citation__ = "doi.org/10.1038/s42003-022-03575-7"
