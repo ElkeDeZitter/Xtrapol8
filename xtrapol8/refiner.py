98a789c98dd1f899672ee7eb89ec99afc7404de8
Paul Bond <git@paulsbond.co.uk>
2026-02-02 14:29:53 +0000

    Using program modules in refiner

--- xtrapol8/refiner.py
+++ xtrapol8/refiner.py
@@ -79,6 +79,9 @@ from .ddm import Difference_distance_analysis
 from .distance_analysis import Distance_analysis
 from .Fextr_utils import *
 from .master import master_phil
+from .programs.dm import dm
+from .programs.fft import fft
+from .programs.refmac import refmac_for_dm
 from .pymol_visualization import Pymol_movie
 
 Xtrapol8_master_phil = master_phil
@@ -414,128 +417,49 @@ class Refiner(object):
 
         return label
 
-    def write_refmac_for_dm(self, pdb_in):
-        """
-        Write and excecute a bash script to run Refmac in order to get an mtz-file that can be used by dm.
-        """
-
-        additional_lines = ""
-        for cif in self.additional.split():
-            if cif.endswith(".cif"):
-                additional_lines += "LIB_IN %s " % (cif)
-
-        mtz_out = "%s_for_dm.mtz" % (get_name(self.mtz_file))
-        pdb_out = "%s_for_dm.pdb" % (get_name(self.mtz_file))
-        log_file = "%s_refmac_for_dm.log" % (get_name(self.mtz_file))
-
-        F_column_labels = self.get_F_column_labels(self.mtz_file)
-
-        script_out = "launch_refmac_for_dm.sh"
-        i = open(script_out, "w")
-        i.write(
-            "#!/bin/sh\n\
-refmac5 XYZIN %s HKLIN %s XYZOUT %s HKLOUT %s %s<<eof > %s \n\
-LABIN  FP=%s SIGFP=SIG%s FREE=FreeR_flag\n\
-REFI TYPE REST RESI MLKF BREF ISOT METH CGMAT \n\
-nfree include 1 \n\
-make check NONE \n\
-ncyc 0 \n\
-MAPC SHAR \n\
-NOHARVEST \n\
-END \n\
-eof"
-            % (
-                pdb_in,
-                self.mtz_file,
-                pdb_out,
-                mtz_out,
-                additional_lines,
-                log_file,
-                F_column_labels,
-                F_column_labels,
-            )
+    def do_refmac_for_dm(self, pdb_in):
+        "Run Refmac in order to get an mtz-file that can be used by dm."
+        mtz_out, pdb_out = refmac_for_dm(
+            xyzin=pdb_in,
+            hklin=self.mtz_file,
+            libins=self.additional.split(),
+            f_label=self.get_F_column_labels(self.mtz_file),
         )
-
-        i.close()
-        os.system("chmod +x %s" % (script_out))
-        print(
-            "Running refmac with zero cycles to prepare files suitable for running dm afterwards, output written to "
-            "%s. Please wait..." % (log_file)
-        )
-        os.system("./%s" % (script_out))
-
         return mtz_out, pdb_out
 
-    def write_density_modification_script(self, mtz_in, pdb_in, mtz_out, log_file):
+    def do_density_modification(self, mtz_in, pdb_in, mtz_out, log_file):
         """
-        Write script to perform density modification with dm. In order to have the correct columns, the mtz-file
-        should original from refmac.
+        Write script to perform density modification with dm.
+        In order to have the correct columns,
+        the mtz-file should original from refmac.
         """
         solc = self.get_solvent_content(pdb_in)
-
-        F_column_labels = self.get_F_column_labels(
-            self.mtz_file
-        )  # column labels are those inherited from the extrapolated structure factors
-
-        script_out = "launch_dm.sh"
-        i = open(script_out, "w")
-        i.write(
-            "#!/bin/sh \n\
-\n\
-#dm:\n\
-dm hklin %s hklout %s <<eor > %s \n\
-SOLC %.3f\n\
-MODE SOLV HIST MULTI SAYR\n\
-COMBINE %s\n\
-NCYC %d\n\
-LABI FP=%s SIGFP=SIG%s PHIO=PHIC_ALL FOMO=FOM\n\
-LABO FDM=FDM PHIDM=PHIDM\n\
-eor\n"
-            % (
-                mtz_in,
-                mtz_out,
-                log_file,
-                solc,
-                self.density_modification.combine,
-                self.density_modification.cycles,
-                F_column_labels,
-                F_column_labels,
-            )
+        # column labels are those inherited from the extrapolated structure factors
+        F_column_labels = self.get_F_column_labels(self.mtz_file)
+        dm(
+            mtz_in=mtz_in,
+            mtz_out=mtz_out,
+            solc=solc,
+            combine=self.density_modification.combine,
+            cycles=self.density_modification.cycles,
+            f_label=F_column_labels,
+            log_file=log_file,
         )
-
         ccp4_map_name = re.sub(r".mtz$", ".ccp4", mtz_out)
-
-        i.write(
-            "#generate map in ccp4 format\n\
-fft hklin %s mapout %s <<eof > fft.log\n\
-LABI F1=FDM PHI=PHIDM\n\
-eof"
-            % (mtz_out, ccp4_map_name)
-        )
-
-        i.close()
-        os.system("chmod +x %s" % (script_out))
-        return script_out
+        fft(mtz_out, ccp4_map_name, "FDM", "PHIDM")
 
     def ccp4_dm(self, pdb_in):
         """
-        Use dm to perform density modification. In order to get the correct columns, we first run refmac with 0
-        cycles with the refined model from phenix.refine.
+        Use dm to perform density modification.
+        In order to get the correct columns, we first run refmac with 0 cycles
+        with the refined model from phenix.refine.
         """
-        mtz_for_dm, pdb_for_dm = self.write_refmac_for_dm(pdb_in)
-        # mtz_out_dm = re.sub(r"for_dm.mtz$", "dm.mtz", mtz_for_dm)
+        mtz_for_dm, pdb_for_dm = self.do_refmac_for_dm(pdb_in)
         mtz_out_dm = re.sub(r".pdb$", "_dm.mtz", pdb_in)
         if os.path.isfile(mtz_for_dm) and os.path.isfile(pdb_for_dm):
             log_file = re.sub(r".mtz$", ".log", mtz_out_dm)
-            script_dm = self.write_density_modification_script(
-                mtz_for_dm, pdb_for_dm, mtz_out_dm, log_file
-            )
-            print(
-                "Running density modification, output written to %s. Please wait..."
-                % (log_file)
-            )
-            os.system("./%s" % (script_dm))
-
+            LOG.info(f"Running density modification, see {log_file}. Please wait...")
+            self.do_density_modification(mtz_for_dm, pdb_for_dm, mtz_out_dm, log_file)
         return mtz_out_dm
 
     def get_mtz_resolution(self, mtz_in):
